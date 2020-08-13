508c330ec3977f0a997d3e6f8d6a2b427750cfa5
Jan-Lukas Wynen <j-l.wynen@hotmail.de>
2020-08-13 15:50:03 +0200

    Add more info to setup

--- setup.py
+++ setup.py
@@ -1,6 +1,9 @@
+from pathlib import Path
 import re
 from setuptools import setup
 import subprocess
+
+README = (Path(__file__).resolve().parent / "README.md").read_text()
 
 
 def git_version_tag():
@@ -49,21 +52,30 @@ def get_version():
 setup(
     name="pentinsula",
     version=get_version(),
-    packages=["pentinsula"],
-    install_requires=["h5py",
-                      "numpy"],
-
+    description="h5py utilities for time series data in HDF5",
+    long_description=README,
+    long_description_content_type="text/markdown",
     author="Jan-Lukas Wynen",
     author_email="j-l.wynen@hotmail.de",
-    description="h5py utilities for time series data in HDF5",
+    license="MIT",
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3 :: Only",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Intended Audience :: Developers",
+        "Intended Audience :: Science/Research",
+        "Topic :: Scientific/Engineering",
+        "Topic :: Software Development :: Libraries :: Python Modules"
+    ],
     keywords="hdf5 time_series h5py",
     url="https://github.com/jl-wynen/pentinsula",
     project_urls={
         "Source Code": "https://github.com/jl-wynen/pentinsula",
     },
-    # TODO fill in
-    # python_requires=">=",
-    # classifiers=[
-    #
-    # ]
+    packages=["pentinsula"],
+    install_requires=["h5py",
+                      "numpy"],
+    python_requires=">=3.6",
 )
